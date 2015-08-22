70d6ba8df5d093c952678d6d3736e0b6c3d30bde
Michael Leung <mick.leung@gmail.com>
2015-08-22 21:16:02 +1000

    commented out setwd() code

--- codebook_table.R
+++ codebook_table.R
@@ -4,8 +4,8 @@ codebook_table$transformation <- 'Mean of all observations for subject/activity'
 
 codebook_table$unit_of_measure <- 'Standardised between -1 and 1'
 
-codebook_table <- codebook_table[,c(2,4, 5, 3)]
-
 codebook_table$notes <- ''
 
-write.table(codebook_table, 'codebook_table.txt', sep='|', row.names=FALSE, quote=FALSE)
+codebook_table <- codebook_table[,c(2,6, 4, 5, 3)]
+
+write.table(codebook_table, 'codebook_table2.txt', sep='|', row.names=FALSE, quote=FALSE)
--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,40 @@
 #load dplyr
 library(dplyr)
 
+#set working directory (please set to the directory where you have downloaded the data)
+#setwd('/Users/michaelleung/Dropbox/datascience/Coursera/Getting and Cleaning Data/Getting_and_Cleaning_Data_Assignment')
+
 #read in feature names
-setwd('/Users/michaelleung/Dropbox/datascience/Coursera/Getting and Cleaning Data/UCI HAR Dataset/')
 feature_names <- read.table('features.txt', header=FALSE)
 names(feature_names) <- c('feature_id', 'feature.name')
 
-#remove brackets 
-feature_names$feature.name <- gsub('-','_',gsub('[()]', '', feature_names$feature.name))
+#Save original feature names for codebook
+feature_names$original.feature.name <- feature_names$feature.name
+
+#remove commas, brackets and hyphens in feature names, replace with underscore
+feature_names$feature.name <- gsub(',', '_', gsub('-','_',gsub('[()]', '', feature_names$feature.name)))
+
 
 #identify the std deviation and mean measures
-desired_features_list <- grep('*mean*', feature_names$feature.name)
-desired_features_list <- c(desired_features_list, grep('*std*', feature_names$feature.name))
+desired_features_list <- grep('[Mm]ean', feature_names$feature.name)
+desired_features_list <- c(desired_features_list, grep('[Ss]td', feature_names$feature.name))
 desired_features <- subset(feature_names, feature_id %in% desired_features_list)
 
+#Expand abbreviated names to make them more descriptive
+desired_features$feature.name <- gsub('BodyBody', 'Body',gsub('Mag','Magnitude', gsub('Gyro', 'Gyroscope', gsub('Acc', 'Acceleration', desired_features$feature.name))))
 
 #read in test data
-setwd('/Users/michaelleung/Dropbox/datascience/Coursera/Getting and Cleaning Data/UCI HAR Dataset/test')
 test <- read.table('X_test.txt', header=FALSE)
+
+#isolate the desired features
 test <- test[,desired_features$feature_id]
 names(test) <- desired_features$feature.name
+
+#read in subjects
 subject_test <- read.table('subject_test.txt', header=FALSE)
 names(subject_test) <- 'subject'
 
+#read in activity ids
 y_test <- read.table('Y_test.txt', header=FALSE)
 names(y_test) <- 'activity_id'
 
@@ -30,13 +42,17 @@ names(y_test) <- 'activity_id'
 test <- cbind(subject_test, test, y_test)
 
 #read in train data
-setwd('/Users/michaelleung/Dropbox/datascience/Coursera/Getting and Cleaning Data/UCI HAR Dataset/train')
 train <- read.table('X_train.txt', header=FALSE)
+
+#isolate the desired features
 train <- train[,desired_features$feature_id]
 names(train) <- desired_features$feature.name
+
+#read in subjects
 subject_train <- read.table('subject_train.txt', header=FALSE)
 names(subject_train) <- 'subject'
 
+#read in activity ids
 y_train <- read.table('Y_train.txt', header=FALSE)
 names(y_train) <- 'activity_id'
 
@@ -47,7 +63,6 @@ train <- cbind(subject_train, train, y_train)
 combined <- rbind(test, train)
 
 #read in activity labels
-setwd('/Users/michaelleung/Dropbox/datascience/Coursera/Getting and Cleaning Data/UCI HAR Dataset/')
 activity_labels <- read.table('activity_labels.txt', header=FALSE)
 names(activity_labels) <- c('activity_id', 'activity_name')
 
@@ -58,93 +73,100 @@ combined <- merge(combined, activity_labels, by='activity_id', all.x=TRUE)
 rm(list=ls(pattern='[^combined]'))
 
 #reorder columns for neatness (I want activity_id and activity_label to be adjacent)
-combined <- combined[,c(1,82,2:81)]
+combined <- combined[,c(1,89,2:88)]
 
 #now average each variable by subject and activity_label
-#as a note, I generated the summarise code by writing out the table of variable
-#names and then generating the code dynamically in excel
-#lazy, slightly inefficient, but it works:)
 averaged <- 
   combined %>%
   group_by(subject, activity_name) %>%
   summarise(
-    activity_subject_avg__tBodyAcc_mean_X = mean(tBodyAcc_mean_X),
-    activity_subject_avg__tBodyAcc_mean_Y = mean(tBodyAcc_mean_Y),
-    activity_subject_avg__tBodyAcc_mean_Z = mean(tBodyAcc_mean_Z),
-    activity_subject_avg__tBodyAcc_std_X = mean(tBodyAcc_std_X),
-    activity_subject_avg__tBodyAcc_std_Y = mean(tBodyAcc_std_Y),
-    activity_subject_avg__tBodyAcc_std_Z = mean(tBodyAcc_std_Z),
-    activity_subject_avg__tGravityAcc_mean_X = mean(tGravityAcc_mean_X),
-    activity_subject_avg__tGravityAcc_mean_Y = mean(tGravityAcc_mean_Y),
-    activity_subject_avg__tGravityAcc_mean_Z = mean(tGravityAcc_mean_Z),
-    activity_subject_avg__tGravityAcc_std_X = mean(tGravityAcc_std_X),
-    activity_subject_avg__tGravityAcc_std_Y = mean(tGravityAcc_std_Y),
-    activity_subject_avg__tGravityAcc_std_Z = mean(tGravityAcc_std_Z),
-    activity_subject_avg__tBodyAccJerk_mean_X = mean(tBodyAccJerk_mean_X),
-    activity_subject_avg__tBodyAccJerk_mean_Y = mean(tBodyAccJerk_mean_Y),
-    activity_subject_avg__tBodyAccJerk_mean_Z = mean(tBodyAccJerk_mean_Z),
-    activity_subject_avg__tBodyAccJerk_std_X = mean(tBodyAccJerk_std_X),
-    activity_subject_avg__tBodyAccJerk_std_Y = mean(tBodyAccJerk_std_Y),
-    activity_subject_avg__tBodyAccJerk_std_Z = mean(tBodyAccJerk_std_Z),
-    activity_subject_avg__tBodyGyro_mean_X = mean(tBodyGyro_mean_X),
-    activity_subject_avg__tBodyGyro_mean_Y = mean(tBodyGyro_mean_Y),
-    activity_subject_avg__tBodyGyro_mean_Z = mean(tBodyGyro_mean_Z),
-    activity_subject_avg__tBodyGyro_std_X = mean(tBodyGyro_std_X),
-    activity_subject_avg__tBodyGyro_std_Y = mean(tBodyGyro_std_Y),
-    activity_subject_avg__tBodyGyro_std_Z = mean(tBodyGyro_std_Z),
-    activity_subject_avg__tBodyGyroJerk_mean_X = mean(tBodyGyroJerk_mean_X),
-    activity_subject_avg__tBodyGyroJerk_mean_Y = mean(tBodyGyroJerk_mean_Y),
-    activity_subject_avg__tBodyGyroJerk_mean_Z = mean(tBodyGyroJerk_mean_Z),
-    activity_subject_avg__tBodyGyroJerk_std_X = mean(tBodyGyroJerk_std_X),
-    activity_subject_avg__tBodyGyroJerk_std_Y = mean(tBodyGyroJerk_std_Y),
-    activity_subject_avg__tBodyGyroJerk_std_Z = mean(tBodyGyroJerk_std_Z),
-    activity_subject_avg__tBodyAccMag_mean = mean(tBodyAccMag_mean),
-    activity_subject_avg__tBodyAccMag_std = mean(tBodyAccMag_std),
-    activity_subject_avg__tGravityAccMag_mean = mean(tGravityAccMag_mean),
-    activity_subject_avg__tGravityAccMag_std = mean(tGravityAccMag_std),
-    activity_subject_avg__tBodyAccJerkMag_mean = mean(tBodyAccJerkMag_mean),
-    activity_subject_avg__tBodyAccJerkMag_std = mean(tBodyAccJerkMag_std),
-    activity_subject_avg__tBodyGyroMag_mean = mean(tBodyGyroMag_mean),
-    activity_subject_avg__tBodyGyroMag_std = mean(tBodyGyroMag_std),
-    activity_subject_avg__tBodyGyroJerkMag_mean = mean(tBodyGyroJerkMag_mean),
-    activity_subject_avg__tBodyGyroJerkMag_std = mean(tBodyGyroJerkMag_std),
-    activity_subject_avg__fBodyAcc_mean_X = mean(fBodyAcc_mean_X),
-    activity_subject_avg__fBodyAcc_mean_Y = mean(fBodyAcc_mean_Y),
-    activity_subject_avg__fBodyAcc_mean_Z = mean(fBodyAcc_mean_Z),
-    activity_subject_avg__fBodyAcc_std_X = mean(fBodyAcc_std_X),
-    activity_subject_avg__fBodyAcc_std_Y = mean(fBodyAcc_std_Y),
-    activity_subject_avg__fBodyAcc_std_Z = mean(fBodyAcc_std_Z),
-    activity_subject_avg__fBodyAcc_meanFreq_X = mean(fBodyAcc_meanFreq_X),
-    activity_subject_avg__fBodyAcc_meanFreq_Y = mean(fBodyAcc_meanFreq_Y),
-    activity_subject_avg__fBodyAcc_meanFreq_Z = mean(fBodyAcc_meanFreq_Z),
-    activity_subject_avg__fBodyAccJerk_mean_X = mean(fBodyAccJerk_mean_X),
-    activity_subject_avg__fBodyAccJerk_mean_Y = mean(fBodyAccJerk_mean_Y),
-    activity_subject_avg__fBodyAccJerk_mean_Z = mean(fBodyAccJerk_mean_Z),
-    activity_subject_avg__fBodyAccJerk_std_X = mean(fBodyAccJerk_std_X),
-    activity_subject_avg__fBodyAccJerk_std_Y = mean(fBodyAccJerk_std_Y),
-    activity_subject_avg__fBodyAccJerk_std_Z = mean(fBodyAccJerk_std_Z),
-    activity_subject_avg__fBodyAccJerk_meanFreq_X = mean(fBodyAccJerk_meanFreq_X),
-    activity_subject_avg__fBodyAccJerk_meanFreq_Y = mean(fBodyAccJerk_meanFreq_Y),
-    activity_subject_avg__fBodyAccJerk_meanFreq_Z = mean(fBodyAccJerk_meanFreq_Z),
-    activity_subject_avg__fBodyGyro_mean_X = mean(fBodyGyro_mean_X),
-    activity_subject_avg__fBodyGyro_mean_Y = mean(fBodyGyro_mean_Y),
-    activity_subject_avg__fBodyGyro_mean_Z = mean(fBodyGyro_mean_Z),
-    activity_subject_avg__fBodyGyro_std_X = mean(fBodyGyro_std_X),
-    activity_subject_avg__fBodyGyro_std_Y = mean(fBodyGyro_std_Y),
-    activity_subject_avg__fBodyGyro_std_Z = mean(fBodyGyro_std_Z),
-    activity_subject_avg__fBodyGyro_meanFreq_X = mean(fBodyGyro_meanFreq_X),
-    activity_subject_avg__fBodyGyro_meanFreq_Y = mean(fBodyGyro_meanFreq_Y),
-    activity_subject_avg__fBodyGyro_meanFreq_Z = mean(fBodyGyro_meanFreq_Z),
-    activity_subject_avg__fBodyAccMag_mean = mean(fBodyAccMag_mean),
-    activity_subject_avg__fBodyAccMag_std = mean(fBodyAccMag_std),
-    activity_subject_avg__fBodyAccMag_meanFreq = mean(fBodyAccMag_meanFreq),
-    activity_subject_avg__fBodyBodyAccJerkMag_mean = mean(fBodyBodyAccJerkMag_mean),
-    activity_subject_avg__fBodyBodyAccJerkMag_std = mean(fBodyBodyAccJerkMag_std),
-    activity_subject_avg__fBodyBodyAccJerkMag_meanFreq = mean(fBodyBodyAccJerkMag_meanFreq),
-    activity_subject_avg__fBodyBodyGyroMag_mean = mean(fBodyBodyGyroMag_mean),
-    activity_subject_avg__fBodyBodyGyroMag_std = mean(fBodyBodyGyroMag_std),
-    activity_subject_avg__fBodyBodyGyroMag_meanFreq = mean(fBodyBodyGyroMag_meanFreq),
-    activity_subject_avg__fBodyBodyGyroJerkMag_mean = mean(fBodyBodyGyroJerkMag_mean),
-    activity_subject_avg__fBodyBodyGyroJerkMag_std = mean(fBodyBodyGyroJerkMag_std),
-    activity_subject_avg__fBodyBodyGyroJerkMag_meanFreq = mean(fBodyBodyGyroJerkMag_meanFreq)
+    activity_subject_avg__tBodyAcceleration_mean_X = mean(tBodyAcceleration_mean_X),
+    activity_subject_avg__tBodyAcceleration_mean_Y = mean(tBodyAcceleration_mean_Y),
+    activity_subject_avg__tBodyAcceleration_mean_Z = mean(tBodyAcceleration_mean_Z),
+    activity_subject_avg__tBodyAcceleration_std_X = mean(tBodyAcceleration_std_X),
+    activity_subject_avg__tBodyAcceleration_std_Y = mean(tBodyAcceleration_std_Y),
+    activity_subject_avg__tBodyAcceleration_std_Z = mean(tBodyAcceleration_std_Z),
+    activity_subject_avg__tGravityAcceleration_mean_X = mean(tGravityAcceleration_mean_X),
+    activity_subject_avg__tGravityAcceleration_mean_Y = mean(tGravityAcceleration_mean_Y),
+    activity_subject_avg__tGravityAcceleration_mean_Z = mean(tGravityAcceleration_mean_Z),
+    activity_subject_avg__tGravityAcceleration_std_X = mean(tGravityAcceleration_std_X),
+    activity_subject_avg__tGravityAcceleration_std_Y = mean(tGravityAcceleration_std_Y),
+    activity_subject_avg__tGravityAcceleration_std_Z = mean(tGravityAcceleration_std_Z),
+    activity_subject_avg__tBodyAccelerationJerk_mean_X = mean(tBodyAccelerationJerk_mean_X),
+    activity_subject_avg__tBodyAccelerationJerk_mean_Y = mean(tBodyAccelerationJerk_mean_Y),
+    activity_subject_avg__tBodyAccelerationJerk_mean_Z = mean(tBodyAccelerationJerk_mean_Z),
+    activity_subject_avg__tBodyAccelerationJerk_std_X = mean(tBodyAccelerationJerk_std_X),
+    activity_subject_avg__tBodyAccelerationJerk_std_Y = mean(tBodyAccelerationJerk_std_Y),
+    activity_subject_avg__tBodyAccelerationJerk_std_Z = mean(tBodyAccelerationJerk_std_Z),
+    activity_subject_avg__tBodyGyroscope_mean_X = mean(tBodyGyroscope_mean_X),
+    activity_subject_avg__tBodyGyroscope_mean_Y = mean(tBodyGyroscope_mean_Y),
+    activity_subject_avg__tBodyGyroscope_mean_Z = mean(tBodyGyroscope_mean_Z),
+    activity_subject_avg__tBodyGyroscope_std_X = mean(tBodyGyroscope_std_X),
+    activity_subject_avg__tBodyGyroscope_std_Y = mean(tBodyGyroscope_std_Y),
+    activity_subject_avg__tBodyGyroscope_std_Z = mean(tBodyGyroscope_std_Z),
+    activity_subject_avg__tBodyGyroscopeJerk_mean_X = mean(tBodyGyroscopeJerk_mean_X),
+    activity_subject_avg__tBodyGyroscopeJerk_mean_Y = mean(tBodyGyroscopeJerk_mean_Y),
+    activity_subject_avg__tBodyGyroscopeJerk_mean_Z = mean(tBodyGyroscopeJerk_mean_Z),
+    activity_subject_avg__tBodyGyroscopeJerk_std_X = mean(tBodyGyroscopeJerk_std_X),
+    activity_subject_avg__tBodyGyroscopeJerk_std_Y = mean(tBodyGyroscopeJerk_std_Y),
+    activity_subject_avg__tBodyGyroscopeJerk_std_Z = mean(tBodyGyroscopeJerk_std_Z),
+    activity_subject_avg__tBodyAccelerationMagnitude_mean = mean(tBodyAccelerationMagnitude_mean),
+    activity_subject_avg__tBodyAccelerationMagnitude_std = mean(tBodyAccelerationMagnitude_std),
+    activity_subject_avg__tGravityAccelerationMagnitude_mean = mean(tGravityAccelerationMagnitude_mean),
+    activity_subject_avg__tGravityAccelerationMagnitude_std = mean(tGravityAccelerationMagnitude_std),
+    activity_subject_avg__tBodyAccelerationJerkMagnitude_mean = mean(tBodyAccelerationJerkMagnitude_mean),
+    activity_subject_avg__tBodyAccelerationJerkMagnitude_std = mean(tBodyAccelerationJerkMagnitude_std),
+    activity_subject_avg__tBodyGyroscopeMagnitude_mean = mean(tBodyGyroscopeMagnitude_mean),
+    activity_subject_avg__tBodyGyroscopeMagnitude_std = mean(tBodyGyroscopeMagnitude_std),
+    activity_subject_avg__tBodyGyroscopeJerkMagnitude_mean = mean(tBodyGyroscopeJerkMagnitude_mean),
+    activity_subject_avg__tBodyGyroscopeJerkMagnitude_std = mean(tBodyGyroscopeJerkMagnitude_std),
+    activity_subject_avg__fBodyAcceleration_mean_X = mean(fBodyAcceleration_mean_X),
+    activity_subject_avg__fBodyAcceleration_mean_Y = mean(fBodyAcceleration_mean_Y),
+    activity_subject_avg__fBodyAcceleration_mean_Z = mean(fBodyAcceleration_mean_Z),
+    activity_subject_avg__fBodyAcceleration_std_X = mean(fBodyAcceleration_std_X),
+    activity_subject_avg__fBodyAcceleration_std_Y = mean(fBodyAcceleration_std_Y),
+    activity_subject_avg__fBodyAcceleration_std_Z = mean(fBodyAcceleration_std_Z),
+    activity_subject_avg__fBodyAcceleration_meanFreq_X = mean(fBodyAcceleration_meanFreq_X),
+    activity_subject_avg__fBodyAcceleration_meanFreq_Y = mean(fBodyAcceleration_meanFreq_Y),
+    activity_subject_avg__fBodyAcceleration_meanFreq_Z = mean(fBodyAcceleration_meanFreq_Z),
+    activity_subject_avg__fBodyAccelerationJerk_mean_X = mean(fBodyAccelerationJerk_mean_X),
+    activity_subject_avg__fBodyAccelerationJerk_mean_Y = mean(fBodyAccelerationJerk_mean_Y),
+    activity_subject_avg__fBodyAccelerationJerk_mean_Z = mean(fBodyAccelerationJerk_mean_Z),
+    activity_subject_avg__fBodyAccelerationJerk_std_X = mean(fBodyAccelerationJerk_std_X),
+    activity_subject_avg__fBodyAccelerationJerk_std_Y = mean(fBodyAccelerationJerk_std_Y),
+    activity_subject_avg__fBodyAccelerationJerk_std_Z = mean(fBodyAccelerationJerk_std_Z),
+    activity_subject_avg__fBodyAccelerationJerk_meanFreq_X = mean(fBodyAccelerationJerk_meanFreq_X),
+    activity_subject_avg__fBodyAccelerationJerk_meanFreq_Y = mean(fBodyAccelerationJerk_meanFreq_Y),
+    activity_subject_avg__fBodyAccelerationJerk_meanFreq_Z = mean(fBodyAccelerationJerk_meanFreq_Z),
+    activity_subject_avg__fBodyGyroscope_mean_X = mean(fBodyGyroscope_mean_X),
+    activity_subject_avg__fBodyGyroscope_mean_Y = mean(fBodyGyroscope_mean_Y),
+    activity_subject_avg__fBodyGyroscope_mean_Z = mean(fBodyGyroscope_mean_Z),
+    activity_subject_avg__fBodyGyroscope_std_X = mean(fBodyGyroscope_std_X),
+    activity_subject_avg__fBodyGyroscope_std_Y = mean(fBodyGyroscope_std_Y),
+    activity_subject_avg__fBodyGyroscope_std_Z = mean(fBodyGyroscope_std_Z),
+    activity_subject_avg__fBodyGyroscope_meanFreq_X = mean(fBodyGyroscope_meanFreq_X),
+    activity_subject_avg__fBodyGyroscope_meanFreq_Y = mean(fBodyGyroscope_meanFreq_Y),
+    activity_subject_avg__fBodyGyroscope_meanFreq_Z = mean(fBodyGyroscope_meanFreq_Z),
+    activity_subject_avg__fBodyAccelerationMagnitude_mean = mean(fBodyAccelerationMagnitude_mean),
+    activity_subject_avg__fBodyAccelerationMagnitude_std = mean(fBodyAccelerationMagnitude_std),
+    activity_subject_avg__fBodyAccelerationMagnitude_meanFreq = mean(fBodyAccelerationMagnitude_meanFreq),
+    activity_subject_avg__fBodyAccelerationJerkMagnitude_mean = mean(fBodyAccelerationJerkMagnitude_mean),
+    activity_subject_avg__fBodyAccelerationJerkMagnitude_std = mean(fBodyAccelerationJerkMagnitude_std),
+    activity_subject_avg__fBodyAccelerationJerkMagnitude_meanFreq = mean(fBodyAccelerationJerkMagnitude_meanFreq),
+    activity_subject_avg__fBodyGyroscopeMagnitude_mean = mean(fBodyGyroscopeMagnitude_mean),
+    activity_subject_avg__fBodyGyroscopeMagnitude_std = mean(fBodyGyroscopeMagnitude_std),
+    activity_subject_avg__fBodyGyroscopeMagnitude_meanFreq = mean(fBodyGyroscopeMagnitude_meanFreq),
+    activity_subject_avg__fBodyGyroscopeJerkMagnitude_mean = mean(fBodyGyroscopeJerkMagnitude_mean),
+    activity_subject_avg__fBodyGyroscopeJerkMagnitude_std = mean(fBodyGyroscopeJerkMagnitude_std),
+    activity_subject_avg__fBodyGyroscopeJerkMagnitude_meanFreq = mean(fBodyGyroscopeJerkMagnitude_meanFreq),
+    activity_subject_avg__angletBodyAccelerationMean_gravity = mean(angletBodyAccelerationMean_gravity),
+    activity_subject_avg__angletBodyAccelerationJerkMean_gravityMean = mean(angletBodyAccelerationJerkMean_gravityMean),
+    activity_subject_avg__angletBodyGyroscopeMean_gravityMean = mean(angletBodyGyroscopeMean_gravityMean),
+    activity_subject_avg__angletBodyGyroscopeJerkMean_gravityMean = mean(angletBodyGyroscopeJerkMean_gravityMean),
+    activity_subject_avg__angleX_gravityMean = mean(angleX_gravityMean),
+    activity_subject_avg__angleY_gravityMean = mean(angleY_gravityMean),
+    activity_subject_avg__angleZ_gravityMean = mean(angleZ_gravityMean)
   )
+
+#output data to text file
+write.table(averaged, 'tidy_dataset.txt', row.names=FALSE)
